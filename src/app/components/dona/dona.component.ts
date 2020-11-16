241bfdcb4d2a9b42b41a43ab03981d9dbc50c78b
Alex Gabriel Yovera Loayza <alexgabriel096@gmail.com>
2020-11-15 23:38:11 -0500

    libreria de graficos en angular

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,40 @@
+import { Component  } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styleUrls: ['./dona.component.css']
+})
+export class DonaComponent {
+
+ // Doughnut
+ public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+ public doughnutChartData: MultiDataSet = [
+   [350, 450, 100]
+ ];
+ public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public numerosRandom(){
+
+    this.doughnutChartData = [
+          [
+            Math.round( Math.random()*100 ),
+            Math.round( Math.random()*100 ),
+            Math.round( Math.random()*100 )
+          ]
+    ];
+
+  }
+
+}
